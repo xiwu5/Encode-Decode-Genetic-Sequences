1338f5b668cff72b137b160bffb9bc616132f897
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2025-05-22 11:44:05 -0700

    Remove aliasing function name in part 2 tests

--- tests/test_part_two.py
+++ tests/test_part_two.py
@@ -1,42 +1,42 @@
-from source.part_two import categorize_strand as categorize_encoded_strand
+from source.part_two import categorize_strand
 from .test_data import DNA_ENCODED, RNA_ENCODED, ENCODED_BOTH_BASES, ENCODED_NEITHER_BASE
 
-def test_categorize_encoded_strand_DNA():
+def test_encoded_categorize_strand_DNA():
     # Arrange
     expected = 0
 
     # Act
-    result = categorize_encoded_strand(DNA_ENCODED)
+    result = categorize_strand(DNA_ENCODED)
 
     # Assert
     assert result == expected, f"Expected {expected}, but got {result}"
 
-def test_categorize_encoded_strand_RNA():
+def test_encoded_categorize_strand_RNA():
     # Arrange
     expected = 1
 
     # Act
-    result = categorize_encoded_strand(RNA_ENCODED)
+    result = categorize_strand(RNA_ENCODED)
 
     # Assert
     assert result == expected, f"Expected {expected}, but got {result}"
 
-def test_categorize_encoded_strand_both_bases():
+def test_encoded_categorize_strand_both_bases():
     # Arrange
     expected = -1
 
     # Act
-    result = categorize_encoded_strand(ENCODED_BOTH_BASES)
+    result = categorize_strand(ENCODED_BOTH_BASES)
 
     # Assert
     assert result == expected, f"Expected {expected}, but got {result}"
 
-def test_categorize_encoded_strand_neither_base():
+def test_encoded_categorize_strand_neither_base():
     # Arrange
     expected = -1
 
     # Act
-    result = categorize_encoded_strand(ENCODED_NEITHER_BASE)
+    result = categorize_strand(ENCODED_NEITHER_BASE)
 
     # Assert
     assert result == expected, f"Expected {expected}, but got {result}"
